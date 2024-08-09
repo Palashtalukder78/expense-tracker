0b1ace899740799b11719d48681c0e1c2340f887
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-08-09 11:12:37 +0600

    UI setup

---
+++ client-site/src/components/Transactions/Transaction.jsx
@@ -0,0 +1,20 @@
+
+
+const Transaction = () => {
+    return (
+      <li className="transaction income">
+        <p>Earned this month</p>
+        <div className="right">
+          <p>৳ 100</p>
+          <button className="link">
+            <img className="icon" src="./images/edit.svg" />
+          </button>
+          <button className="link">
+            <img className="icon" src="./images/delete.svg" />
+          </button>
+        </div>
+      </li>
+    );
+};
+
+export default Transaction;
